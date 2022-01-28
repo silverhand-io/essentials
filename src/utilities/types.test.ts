208c8c59bbf77cbe6313a22139ab834a146c906d
Charles Zhao <charleszhao@silverhand.io>
2022-01-28 20:29:24 +0800

    test: add snake case to camel case type keys conversion test

---
+++ src/utilities/types.test.ts
@@ -0,0 +1,20 @@
+import { expectTypeOf } from 'expect-type';
+
+import { KeysToCamelCase } from '.';
+
+describe('Check type conversion', () => {
+  test('Snake case to camel case key mapping conversion', () => {
+    type SnakeCaseKeyType = {
+      key_of_string: string;
+      key_of_number: number;
+    };
+
+    type CamelCaseKeyType = {
+      keyOfString: string;
+      keyOfNumber: number;
+    };
+
+    expectTypeOf<SnakeCaseKeyType>().not.toEqualTypeOf<CamelCaseKeyType>();
+    expectTypeOf<KeysToCamelCase<SnakeCaseKeyType>>().toEqualTypeOf<CamelCaseKeyType>();
+  });
+});
