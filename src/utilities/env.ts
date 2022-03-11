dbbfddd11f662770c32191eaffde02e9f0612355
Gao Sun <gao@silverhand.io>
2022-03-11 16:17:21 +0800

    fix: treat `process` as optional

--- src/utilities/env.ts
+++ src/utilities/env.ts
@@ -1,9 +1,9 @@
 import { assert } from './assert';
 
-export const getEnv = (key: string, fallback = ''): string => process.env[key] ?? fallback;
+export const getEnv = (key: string, fallback = ''): string => process?.env[key] ?? fallback;
 
 export const assertEnv = (key: string): string => {
-  const value = process.env[key];
+  const value = process?.env[key];
   assert(value, new Error(`env variable ${key} not found`));
 
   return value;
