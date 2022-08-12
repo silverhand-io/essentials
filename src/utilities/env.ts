1118a3805d9c2859415c9d3d1ffc4d081e05f7cc
Xiao Yijun <xiaoyijun@silverhand.io>
2022-08-12 11:44:27 +0800

    feat: `getEnvAsStringArray`

--- src/utilities/env.ts
+++ src/utilities/env.ts
@@ -2,6 +2,19 @@ import { assert } from './assert';
 
 export const getEnv = (key: string, fallback = ''): string => process?.env[key] ?? fallback;
 
+export const getEnvAsStringArray = (envKey: string, fallback: string[] = []): string[] => {
+  const rawValue = getEnv(envKey);
+
+  if (!rawValue) {
+    return fallback;
+  }
+
+  return rawValue
+    .split(',')
+    .map((value) => value.trim())
+    .filter(Boolean);
+};
+
 export const assertEnv = (key: string): string => {
   const value = process?.env[key];
   assert(value, new Error(`env variable ${key} not found`));
