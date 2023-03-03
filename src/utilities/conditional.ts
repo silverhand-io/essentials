11649e697c351098bc98cc1eed89833b96a7565d
Gao Sun <gao@silverhand.io>
2023-03-03 23:12:47 +0800

    feat: conditionalArray()

--- src/utilities/conditional.ts
+++ src/utilities/conditional.ts
@@ -1,8 +1,11 @@
 import { notFalsy } from './assertions.js';
-import type { Nullable, Optional, Truthy } from './types.js';
+import type { Falsy, Nullable, Optional, Truthy } from './types.js';
 
 export const nullable = <T>(exp: T): Nullable<Truthy<T>> => (notFalsy(exp) ? exp : null);
 
 export const conditional = <T>(exp: T): Optional<Truthy<T>> => (notFalsy(exp) ? exp : undefined);
 
 export const conditionalString = <T>(exp: T): string => (notFalsy(exp) ? String(exp) : '');
+
+export const conditionalArray = <T>(...exp: Array<T | Falsy>): T[] =>
+  exp.filter((value): value is Exclude<T, Falsy> => notFalsy(value));
