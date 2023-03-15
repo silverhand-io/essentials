d6f186642dd3d0ef05c1ef22c7eec11d43abbaff
Gao Sun <gao@silverhand.io>
2023-03-15 14:19:10 +0800

    refactor: call `.flat()` in `.conditionalArray()`

--- src/utilities/conditional.ts
+++ src/utilities/conditional.ts
@@ -7,5 +7,7 @@ export const conditional = <T>(exp: T): Optional<Truthy<T>> => (notFalsy(exp) ? 
 
 export const conditionalString = <T>(exp: T): string => (notFalsy(exp) ? String(exp) : '');
 
-export const conditionalArray = <T>(...exp: Array<T | Falsy>): T[] =>
-  exp.filter((value): value is Exclude<T, Falsy> => notFalsy(value));
+export const conditionalArray = <T>(
+  ...exp: Readonly<Array<T | Falsy>>
+): Array<T extends Readonly<Array<infer InnerArray>> ? InnerArray : T> =>
+  exp.filter((value): value is Exclude<typeof value, Falsy> => notFalsy(value)).flat();
