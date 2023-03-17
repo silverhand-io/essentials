8d223c521ff7f836db6540473af87eb0e6473479
Gao Sun <gao@silverhand.io>
2023-03-18 02:05:43 +0800

    feat: `tryThat()`

--- src/utilities/function.test.ts
+++ src/utilities/function.test.ts
@@ -1,6 +1,81 @@
-import { trySafe } from './function.js';
+import { isPromise, noop, trySafe, tryThat } from './function.js';
 
 const { jest } = import.meta;
+
+describe('noop', () => {
+  it('should not explode', () => {
+    // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+    expect(noop()).toBe(undefined);
+  });
+});
+
+describe('isPromise()', () => {
+  it('should return true for Promise or Promise-like', () => {
+    expect(isPromise(Promise.resolve())).toBe(true);
+    // eslint-disable-next-line unicorn/no-thenable
+    expect(isPromise({ then: () => true })).toBe(true);
+
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    const promiseLike = function () {
+      return false;
+    };
+    // eslint-disable-next-line @silverhand/fp/no-mutating-assign, unicorn/no-thenable
+    Object.assign(promiseLike, { then: () => true });
+
+    expect(isPromise(promiseLike)).toBe(true);
+  });
+
+  it('should return false for non-Promise', () => {
+    expect(isPromise({})).toBe(false);
+    expect(isPromise(true)).toBe(false);
+  });
+});
+
+describe('tryThat()', () => {
+  it('should directly execute and return or throw if the function is not a Promise', () => {
+    expect(tryThat(() => 'foo', new Error('try'))).toStrictEqual('foo');
+    expect(() =>
+      tryThat(() => {
+        throw new Error('Test');
+      }, new Error('try'))
+    ).toThrowError(new Error('try'));
+    expect(() =>
+      tryThat(
+        () => {
+          throw new Error('Test');
+        },
+        (error) => {
+          throw new Error(String(error instanceof Error && error.message) + ' try');
+        }
+      )
+    ).toThrowError(new Error('Test try'));
+  });
+
+  it('should execute or unwrap a Promise and throw the error', async () => {
+    expect(
+      await tryThat(
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve('bar');
+          }, 0);
+        }),
+        new Error('try')
+      )
+    ).toStrictEqual('bar');
+
+    await expect(
+      tryThat(
+        async () =>
+          new Promise((resolve, reject) => {
+            reject();
+          }),
+        () => {
+          throw new Error('try');
+        }
+      )
+    ).rejects.toStrictEqual(new Error('try'));
+  });
+});
 
 describe('trySafe()', () => {
   it('should directly execute and return if the function is not a Promise', () => {
--- src/utilities/function.ts
+++ src/utilities/function.ts
@@ -8,6 +8,35 @@ export const isPromise = (value: unknown): value is Promise<unknown> =>
   'then' in value &&
   typeof value.then === 'function';
 
+export type TryThat = {
+  <T>(exec: () => T, onError: Error | ((error: unknown) => never)): T;
+  <T>(
+    exec: Promise<T> | (() => Promise<T>),
+    onError: Error | ((error: unknown) => never)
+  ): Promise<T>;
+};
+
+export const tryThat: TryThat = (exec, onError) => {
+  const handleError = (error: unknown) => {
+    if (onError instanceof Error) {
+      throw onError;
+    }
+
+    return onError(error);
+  };
+
+  try {
+    const unwrapped = typeof exec === 'function' ? exec() : exec;
+
+    return isPromise(unwrapped)
+      ? // eslint-disable-next-line promise/prefer-await-to-then
+        unwrapped.catch(handleError)
+      : unwrapped;
+  } catch (error: unknown) {
+    return handleError(error);
+  }
+};
+
 export type TrySafe = {
   <T>(exec: Promise<T> | (() => Promise<T>), onError?: (error: unknown) => void): Promise<
     T | undefined
