d0ee7c827bf304e33ccad48dee59bc0b230bb006
Gao Sun <gao@silverhand.io>
2023-04-25 00:14:47 +0800

    refactor: add CommonJS export

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { summary } from 'rollup-plugin-summary';
+
+/**
+ * @type {import('rollup').OutputOptions}
+ */
+const configs = {
+  input: ['src/index.ts'],
+  output: [
+    {
+      format: 'cjs',
+      dir: 'lib',
+      preserveModules: true,
+      exports: 'named',
+      entryFileNames: '[name].cjs',
+      interop: 'auto',
+    },
+    { dir: 'lib', preserveModules: true },
+  ],
+  plugins: [typescript({ tsconfig: 'tsconfig.build.json' }), nodeResolve(), summary()],
+};
+
+export default configs;
