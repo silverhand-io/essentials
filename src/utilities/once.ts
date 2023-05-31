64f77cf31c888fe39c43a1813392110bf929053f
Gao Sun <gao@silverhand.io>
2023-05-31 15:18:10 +0800

    refactor: add parameter signature

--- src/utilities/once.ts
+++ src/utilities/once.ts
@@ -1,12 +1,11 @@
-type Procedure<T> = (...args: unknown[]) => T;
+type Procedure<A extends unknown[], T> = (...args: A) => T;
 
-/* eslint-disable @silverhand/fp/no-let */
-/* eslint-disable @silverhand/fp/no-mutation */
-export function once<T>(function_: Procedure<T>): Procedure<T> {
+/* eslint-disable @silverhand/fp/no-let, @silverhand/fp/no-mutation */
+export function once<A extends unknown[], T>(function_: Procedure<A, T>): Procedure<A, T> {
   let called = false;
   let result: T;
 
-  return function (this: unknown, ...args: unknown[]) {
+  return function (this: unknown, ...args: A) {
     if (!called) {
       called = true;
       result = function_.apply(this, args);
@@ -15,5 +14,4 @@ export function once<T>(function_: Procedure<T>): Procedure<T> {
     return result;
   };
 }
-/* eslint-enable @silverhand/fp/no-mutation */
-/* eslint-enable @silverhand/fp/no-let */
+/* eslint-enable @silverhand/fp/no-mutation, @silverhand/fp/no-let */
