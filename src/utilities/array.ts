a1e440c165c9f3d74767c07c6ad40dad24e5a633
Gao Sun <gao@silverhand.io>
2024-05-16 10:37:10 +0800

    feat: add `deduplicateByKey`

--- src/utilities/array.ts
+++ src/utilities/array.ts
@@ -20,3 +20,17 @@ export const repeat = <T>(times: number, initial: T, iterate: (accumulator: T) =
 /* eslint-enable @silverhand/fp/no-let, @silverhand/fp/no-mutation */
 
 export const deduplicate = <T>(array: T[]) => [...new Set(array)];
+
+/**
+ * Deduplicate an array of objects by a specific key.
+ *
+ * @param array The array of objects to deduplicate.
+ * @param key The key to deduplicate by.
+ * @example
+ * const array = [{ id: 1, name: 'John' }, { id: 2, name: 'Doe' }, { id: 1, name: 'John' }];
+ * deduplicateByKey(array, 'id');
+ * // Output: [{ id: 1, name: 'John' }, { id: 2, name: 'Doe' }]
+ */
+export const deduplicateByKey = <T>(array: T[], key: keyof T) => [
+  ...new Map(array.map((item) => [item[key], item])).values(),
+];
